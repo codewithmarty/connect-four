9bdb81aba63e423b93103c76266894a50ec3bf07
Martin Nicola <lehitutoring@gmail.com>
2023-03-03 09:47:59 -0700

    feat: finished game logic

---
+++ js/conditions.js
@@ -0,0 +1,129 @@
+const winConditions = [
+    {
+        startingIndex: '20',
+        rowInc: 1,
+        colInc: 1,
+    },
+    {
+        startingIndex: '10',
+        rowInc: 1,
+        colInc: 1,
+    },
+    {
+        startingIndex: '00',
+        rowInc: 1,
+        colInc: 1,
+    },
+    {
+        startingIndex: '01',
+        rowInc: 1,
+        colInc: 1,
+    },
+    {
+        startingIndex: '02',
+        rowInc: 1,
+        colInc: 1,
+    },
+    {
+        startingIndex: '03',
+        rowInc: 1,
+        colInc: 1,
+    },
+    {
+        startingIndex: '26',
+        rowInc: 1,
+        colInc: -1,
+    },
+    {
+        startingIndex: '16',
+        rowInc: 1,
+        colInc: -1,
+    },
+    {
+        startingIndex: '06',
+        rowInc: 1,
+        colInc: -1,
+    },
+    {
+        startingIndex: '05',
+        rowInc: 1,
+        colInc: -1,
+    },
+    {
+        startingIndex: '04',
+        rowInc: 1,
+        colInc: -1,
+    },
+    {
+        startingIndex: '03',
+        rowInc: 1,
+        colInc: -1,
+    },
+    {
+        startingIndex: '00',
+        rowInc: 0,
+        colInc: 1,
+    },
+    {
+        startingIndex: '10',
+        rowInc: 0,
+        colInc: 1,
+    },
+    {
+        startingIndex: '20',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '30',
+        rowInc: 0,
+        colInc: 1,
+    },
+    {
+        startingIndex: '40',
+        rowInc: 0,
+        colInc: 1,
+    },
+    {
+        startingIndex: '50',
+        rowInc: 0,
+        colInc: 1,
+    },
+    {
+        startingIndex: '00',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '01',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '02',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '03',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '04',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '05',
+        rowInc: 1,
+        colInc: 0,
+    },
+    {
+        startingIndex: '06',
+        rowInc: 1,
+        colInc: 0,
+    },
+]
+
+export { winConditions }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// <---------------------- Imports ---------------------->
+import { winConditions } from './conditions.js'
+
+/*----- app's state (variables) -----*/
+const board = [
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""],
+    ["","","","","","",""],
+]
+
+let player = "Blue"
+let row, col
+let winner = false
+let count = 0
+let messageEl
+
+// <---------------------- Build UI ---------------------->
+
 document.querySelector('.start-game-btn').addEventListener('click', () => {
     document.querySelector('.info-box').style.display = 'none'
     init()
@@ -5,9 +26,9 @@ document.querySelector('.start-game-btn').addEventListener('click', () => {
 
 function init() {
     buildGameContainer()
+    const cells = document.getElementsByClassName('cell');
+    Array.from(cells).forEach(cell => cell.addEventListener("click", handleClick));
 }
-
-// <---------------------- Build game structure ---------------------->
 
 function buildGameContainer() {
     buildTitle()
@@ -54,7 +75,7 @@ function buildStatusScreen() {
 }
 
 function buildMessage(elem) {
-    const messageEl = document.createElement('div')
+    messageEl = document.createElement('div')
     messageEl.textContent = 'Player Blue Starts'
     messageEl.classList.add('message')
     elem.appendChild(messageEl)
@@ -68,4 +89,70 @@ function createStatusChip(elem) {
     chipEl.style.height = '50px'
     chipEl.style.backgroundColor = 'blue'
     elem.appendChild(chipEl)
+}
+
+/*----- functions -----*/
+
+function handleClick(evt) {
+    if (!winner) {
+        col = evt.target.id[1];
+        if (!colFull(col)) {
+            fillBoard(col, board);
+            render()
+            checkWin()
+            player = player == "Blue" ? "Yellow" : "Blue";
+            if (winner) {
+                messageEl.innerText = `${winner} wins!`
+            } else {
+                messageEl.innerText = `${player}'s turn!`
+                document.querySelector('.chip').style.backgroundColor = player.toLowerCase()
+            }
+        } else messageEl.innerText = `Player ${player}. Column ${parseInt(col) + 1} is full!`
+    }
+}
+
+function fillBoard(col, board) {
+    for (let i = 5; i > -1; i--) {
+        if (!board[i][col]) {
+            row = i
+            board[row][col] = player;
+            return;
+        }
+    }
+}
+
+function render() {
+    let id = row+col
+    if (player == "Blue") document.getElementById(id).style.backgroundColor = "blue"
+    else document.getElementById(id).style.backgroundColor = "yellow"
+}
+
+function checkWin() { 
+    winConditions.forEach(cond => {
+        analyzeBoard("Blue", parseInt(cond.startingIndex[0]), parseInt(cond.startingIndex[1]), cond.rowInc, cond.colInc)
+        analyzeBoard("Yellow", parseInt(cond.startingIndex[0]), parseInt(cond.startingIndex[1]), cond.rowInc, cond.colInc)
+    })
+}
+
+function analyzeBoard(incomingPlayer, startingRow, startingCol, rowInc, colInc) {
+    let [rowNum, colNum] = [startingRow, startingCol];
+    let [nextRow, nextCol] = [rowNum + rowInc, colNum + colInc];
+    while (rowNum >= 0 && colNum >=0 && nextRow <= 5 && nextCol <= 6) {
+        if (board[rowNum][colNum] == board[rowNum + rowInc][colNum + colInc] && board[rowNum][colNum] == incomingPlayer) {
+            count++;
+        }
+        [rowNum, colNum] = [rowNum + rowInc, colNum + colInc];
+        [nextRow, nextCol] = [rowNum + rowInc, colNum + colInc];
+    }
+    if (count == 3) {
+        winner = incomingPlayer
+    }
+    count = 0;
+}
+
+function colFull(col) {
+    for (let i = 0; i < 6; i++) {
+        if (!board[i][col]) return false; 
+    }
+    return true;
 }
